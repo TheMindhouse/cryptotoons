0541dfa3eb25e115978360ff87498c41519c27d5
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-07-24 12:20:28 +0200

    Switch to HTTPS

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   // Browser page title
   PAGE_TITLE: "CryptoToons - Entertoonment on the blockchain",
-  TOON_IMAGE_BASE_URL: "http://217.182.75.93:3100/toon",
+  TOON_IMAGE_BASE_URL: "https://mindhouse.io:3100/toon",
   // Delay to check transactions in ms
   CHECK_TX_DELAY: 3000,
   // Delay to check account address in ms
