b311212f41760a6695eb1675ace98208f9206e02
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-07-29 15:43:09 +0200

    Updated incorrect network info covers the whole page

--- src/components/AccountWidget/NetworkCheck.js
+++ src/components/AccountWidget/NetworkCheck.js
@@ -5,6 +5,8 @@ import withWeb3 from "../../hoc/withWeb3"
 import { CONFIG } from "../../config"
 import { METAMASK_NETWORK_NAMES } from "../../constants/metamask"
 import type { Web3StoreType } from "../../types/Web3StoreType"
+import logoMetamask from "../../assets/images/logo-metamask.svg"
+import bgBlue from "../../assets/images/background-blue.jpg"
 
 type Props = {
   web3Store: Web3StoreType,
@@ -37,10 +39,20 @@ class NetworkCheck extends React.PureComponent<Props, State> {
       METAMASK_NETWORK_NAMES[networkId] || "Unknown Network"
     const desiredNetworkName = METAMASK_NETWORK_NAMES[CONFIG.ETHEREUM_NETWORK]
     return (
-      <div className="NetworkCheck">
+      <div
+        className="NetworkCheck"
+        style={{ backgroundImage: `url(${bgBlue})` }}
+      >
+        <div>
+          <img
+            src={logoMetamask}
+            className="NetworkCheck__logo"
+            alt="MetaMask logo"
+          />
+        </div>
         <span>
-          Your wallet is connected to the {currentNetworkName}. To use
-          CryptoToons, please switch to the <b>{desiredNetworkName}</b>.
+          Your wallet is connected to the {currentNetworkName}.<br />
+          <b>To use CryptoToons, please switch to the {desiredNetworkName}</b>.
         </span>
       </div>
     )
