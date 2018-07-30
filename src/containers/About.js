918aaf4e8c0b7f706e3cf93b5b9a6bc844f06a74
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-07-30 14:40:42 +0200

    About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,8 +2,9 @@
 import * as React from "react"
 import backgroundBlue from "../assets/images/background-blue.jpg"
 import toonsGang from "../assets/images/toons.png"
-import { Col, Row } from "antd"
 import { setDocumentTitle } from "../helpers/utils"
+import howItsMadeImage from "../assets/images/how-its-made.svg"
+import poweredByEthereum from "../assets/images/powered-by-ethereum.png"
 
 type AboutProps = {}
 
@@ -34,49 +35,89 @@ class About extends React.PureComponent<AboutProps> {
           </div>
         </div>
 
+        <div className="containerWrapper containerWrapper--gray">
+          <div className="container text-center">
+            <h1>
+              <b>CryptoToons are digital art collectibles</b>
+            </h1>
+            <h2>with proof of ownership on the Ethereum blockchain</h2>
+          </div>
+        </div>
+
         <div className="containerWrapper">
-          <div className="container container--large-text text-justify">
-            <Row type="flex" justify="center">
-              <Col
-                xs={{ span: 24 }}
-                sm={{ span: 20 }}
-                md={{ span: 16 }}
-                lg={{ span: 14 }}
+          <div className="container container--small container--large-text text-justify">
+            <p>
+              A crypto-collectible is a{" "}
+              <b>cryptographically unique, non-fungible digital asset</b>.
+              Unlike cryptocurrencies, which require all tokens to be identical,
+              each crypto-collectible token is unique or limited in quantity.
+            </p>
+
+            <h2>
+              <b>Entertoonment!</b>
+            </h2>
+            <p>
+              A team from The Netherlands (<a
+                href="https://webcartoonsstudio.com/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <h1 className="text-center">
-                  <b>About CryptoToons</b>
-                </h1>
-                <p>
-                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
-                  zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
-                  bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een
-                  zethaak met letters nam en ze door elkaar husselde om een
-                  font-catalogus te maken. Het heeft niet alleen vijf eeuwen
-                  overleefd maar is ook, vrijwel onveranderd, overgenomen in
-                  elektronische letterzetting. Het is in de jaren '60 populair
-                  geworden met de introductie van Letraset vellen met Lorem
-                  Ipsum passages en meer recentelijk door desktop publishing
-                  software zoals Aldus PageMaker die versies van Lorem Ipsum
-                  bevatten.
-                </p>
-                <p>
-                  In tegenstelling tot wat algemeen aangenomen wordt is Lorem
-                  Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels
-                  in een stuk klassieke latijnse literatuur uit 45 v.Chr. en is
-                  dus meer dan 2000 jaar oud. Richard McClintock, een professor
-                  latijn aan de Hampden-Sydney College in Virginia, heeft één
-                  van de meer obscure latijnse woorden, consectetur, uit een
-                  Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken
-                  naar het woord in de klassieke literatuur de onverdachte bron
-                  ontdekt. Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33
-                  van "de Finibus Bonorum et Malorum" (De uitersten van goed en
-                  kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een
-                  verhandeling over de theorie der ethiek, erg populair tijdens
-                  de renaissance. De eerste regel van Lorem Ipsum, "Lorem ipsum
-                  dolor sit amet..", komt uit een zin in sectie 1.10.32.
-                </p>
-              </Col>
-            </Row>
+                WebCartoons Studio
+              </a>) and Poland (<a
+                href="https://mindhouse.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The Mindhouse
+              </a>) united together to bring you this unique entertoonment on
+              the blockchain, which combines the blockchain with cartoon fun.
+            </p>
+            <p>
+              <b>
+                Our toons are entertainment and how we call it enterTOONment!
+              </b>{" "}
+              When we look at nature, animals and daily human life we can’t stop
+              laughing and creating. So much entertoonment around us! We started
+              CryptoToons to tell people in a fun way more about crypto and the
+              blockchain.
+            </p>
+            <p>Collect, trade or gift your toon to a friend!</p>
+
+            <h2>
+              <b>Toons Creation</b>
+            </h2>
+            <p>
+              The characters are designed by combining different shapes, colors
+              and layers to create a toon. Through combining different layers
+              with each other there are billions of possible toons combinations
+              and they all have their own DNA. This means your toon is always
+              unique.
+            </p>
+            <img
+              src={howItsMadeImage}
+              alt="How the Toon is created"
+              style={{ margin: "50px 0" }}
+            />
+            <p>
+              You can trade or gift your toon to someone else but everyone's
+              collection will be unique. Start collecting your Toons now!
+            </p>
+          </div>
+        </div>
+
+        <div className="containerWrapper">
+          <div className="container text-center">
+            <a
+              href="https://ethereum.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={poweredByEthereum}
+                alt="Powered by Ethereum"
+                style={{ width: 400, maxWidth: "80%", marginBottom: 100 }}
+              />
+            </a>
           </div>
         </div>
       </div>
