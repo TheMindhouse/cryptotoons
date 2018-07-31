a079215f925ecfa32840fb7650c51f8657023218
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-07-31 18:39:37 +0200

    Family card coming soon

--- src/components/FamilyCard/FamilyCard.js
+++ src/components/FamilyCard/FamilyCard.js
@@ -4,6 +4,7 @@ import "./styles/FamilyCard.css"
 import { URLHelper } from "../../helpers/URLhelper"
 import { Link } from "react-router-dom"
 import { FAMILY_NAMES } from "../../constants/toonFamilies"
+import { TOON_CONTRACT_ADDRESSES } from "../../constants/contracts"
 
 type Props = {
   image: string,
@@ -14,8 +15,19 @@ class FamilyCard extends React.PureComponent<Props> {
   static defaultProps = {}
 
   render() {
+    const isFamilyPublished =
+      TOON_CONTRACT_ADDRESSES[this.props.familyId] !== ""
     return (
-      <div className="FamilyCard">
+      <div
+        className={`FamilyCard ${
+          !isFamilyPublished ? "FamilyCard--disabled" : ""
+        }`}
+      >
+        {!isFamilyPublished && (
+          <div className="FamilyCard__ComingSoon">
+            <span>COMING SOON, STAY TOONED!</span>
+          </div>
+        )}
         <Link to={URLHelper.toonFamily(this.props.familyId)}>
           <img
             src={this.props.image}
