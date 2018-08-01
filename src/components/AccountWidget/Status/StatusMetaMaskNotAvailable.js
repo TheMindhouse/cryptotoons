f8d0e6be4f998a8760977b6907301c5ad4973188
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-08-01 19:39:09 +0200

    Fix copy

--- src/components/AccountWidget/Status/StatusMetaMaskNotAvailable.js
+++ src/components/AccountWidget/Status/StatusMetaMaskNotAvailable.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 
 export const StatusMetaMaskNotAvailable = () => (
   <span className="AccountStatus__InstallMetaMaskInfo">
-    To participate in painting and trading CryptoCanvas on this site,{" "}
+    To participate in trading CryptoToons on this site,{" "}
     <Link to={URLHelper.help}>install the MetaMask Chrome plugin</Link>.
   </span>
 )
