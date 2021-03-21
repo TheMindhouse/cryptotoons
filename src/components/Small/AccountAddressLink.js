21365c1b1b0d1ba2fac2927d9a1890d46edc6c96
Chris Gaszynski <k.gaszynski@gmail.com>
2021-03-21 22:25:02 +0200

    Show "CryptoToons Creators" owner for our own toons

--- src/components/Small/AccountAddressLink.js
+++ src/components/Small/AccountAddressLink.js
@@ -1,8 +1,9 @@
 // @flow
 import * as React from "react"
 import { URLHelper } from "../../helpers/URLhelper"
-import { cutAddress } from "../../helpers/strings"
+import { cutAddress, equalStrings } from "../../helpers/strings"
 import { Link } from "react-router-dom"
+import { CONTRACT_OWNER_ADDRESS } from "../../constants/contracts"
 
 type Props = {
   address: string,
@@ -12,9 +13,11 @@ class AccountAddressLink extends React.PureComponent<Props> {
   static defaultProps = {}
 
   render() {
+    const { address } = this.props
+    const isFromCreators = equalStrings(address, CONTRACT_OWNER_ADDRESS)
     return (
-      <Link to={URLHelper.account(this.props.address)}>
-        {cutAddress(this.props.address)}
+      <Link to={URLHelper.account(address)}>
+        {isFromCreators ? "CryptoToons Creators" : cutAddress(address)}
       </Link>
     )
   }
