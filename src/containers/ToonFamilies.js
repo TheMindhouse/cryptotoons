78d0326d83024561468afb60ddc779883e254920
Chris Gaszynski <k.gaszynski@gmail.com>
2021-08-12 23:59:03 +0200

    Add MoonMonkeys to front page

--- src/containers/ToonFamilies.js
+++ src/containers/ToonFamilies.js
@@ -11,8 +11,10 @@ import familyGorillas from "../assets/images/toon-families/gorillas.png"
 import familyHippos from "../assets/images/toon-families/hippos.png"
 import familyLlamas from "../assets/images/toon-families/llamas.png"
 import familyPandas from "../assets/images/toon-families/pandas.png"
+import familyMonkeys from "../assets/images/toon-families/monkeys.png"
 import { FAMILY_IDS } from "../constants/toonFamilies"
 import { setDocumentTitle } from "../helpers/utils"
+import { URLHelper } from "../helpers/URLhelper"
 
 type Props = {}
 
@@ -33,12 +35,18 @@ class ToonFamilies extends React.PureComponent<Props> {
       [FAMILY_IDS.Donkeys, familyDonkeys],
       [FAMILY_IDS.Gorillas, familyGorillas],
       [FAMILY_IDS.Pandas, familyPandas],
+      [FAMILY_IDS.Monkeys, familyMonkeys],
     ]
     return (
       <div className="containerWrapper containerWrapper--gray">
         <div className="container">
+          <h3 style={{ marginBottom: 20 }}>
+            <a href={URLHelper.genesis}>
+              See the remaining Genesis Toons from Creators &raquo;
+            </a>
+          </h3>
           <Row gutter={20}>
-            {families.map((family: [number, string]) => (
+            {families.reverse().map((family: [number, string]) => (
               <Col xs={{ span: 24 }} sm={{ span: 12 }} key={family[0]}>
                 <FamilyCard familyId={family[0]} image={family[1]} />
               </Col>
